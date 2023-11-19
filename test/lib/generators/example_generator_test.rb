e54d2647c1dbe79d031eaa96c91f180921771a78
Thomas Cannon <tcannon00@gmail.com>
2023-11-18 20:03:35 -0500

    Try `ensure_tmp_generators_symlink` patch
    * See: https://github.com/rails/rails/issues/50099#issuecomment-1817705770

--- test/lib/generators/example_generator_test.rb
+++ test/lib/generators/example_generator_test.rb
@@ -4,7 +4,7 @@ require "generators/example/example_generator"
 class ExampleGeneratorTest < Rails::Generators::TestCase
   tests ExampleGenerator
   destination Rails.root.join("tmp/generators")
-  setup :prepare_destination
+  setup :prepare_destination, :ensure_tmp_generators_symlink
 
   test "generator runs without errors" do
     assert_nothing_raised do
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,5 +11,14 @@ module ActiveSupport
     fixtures :all
 
     # Add more helper methods to be used by all tests here...
+
+    def ensure_tmp_generators_symlink
+      return unless Rails.env.test?
+
+      src_bin = File.join(Rails.root, 'bin')
+      dest_bin = File.join(destination_root, 'bin')
+
+      FileUtils.ln_s(src_bin, dest_bin) unless File.symlink?(dest_bin)
+    end
   end
 end
